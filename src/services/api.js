dfb97ea1cfe09a9be5956f475429e366e0a818aa
Lucas Cid <lucasfelipelimacid@gmail.com>
2023-11-19 15:02:10 -0300

    change in url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL : "https://deploy-rocketnotes.onrender.com"
+  baseURL : "https://deploy-rocket-api.onrender.com"
 })
 
 /**
